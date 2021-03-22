6e219a8805d624d2ccf3a5fbdbf1cb8200ac1b7e
GregorySkuza <gskuza94@gmail.com>
2021-03-22 13:00:31 +0100

    Refactor LoginappAplication to LoginApp

--- src/main/java/pl/gskuza/loginapp/LoginApp.java
+++ src/main/java/pl/gskuza/loginapp/LoginApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginappApplication {
+public class LoginApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoginappApplication.class, args);
+        SpringApplication.run(LoginApp.class, args);
     }
 
 }
